b4a899a44591b67e2b62cffaa7521b83abefffbf
najavy.html <najavyuz10@gmail.com>
2024-05-02 12:21:13 -0400

    nueva actualizacion

--- Laravel-D-Antojitos/resources/views/PAGprincipal.blade.php
+++ Laravel-D-Antojitos/resources/views/PAGprincipal.blade.php
@@ -5,13 +5,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>D'antojitos Gourmet Decora</title>
+   
 </head>
 <body>
-  <div class="vertical-menu">
+<style>
+    .vertical-menu {
+        width: 200px; /* Establece un ancho si lo deseas */
+    }
+    .vertical-menu a {
+        background-color: #eee; /* Color de fondo gris */
+        color: black; /* Color de texto negro */
+        display: block; /* Hace que los enlaces aparezcan uno debajo del otro */
+        padding: 12px; /* Agrega un poco de espacio */
+    }
+</style>
+
+<div class="vertical-menu">
     <a href="#">Inicio</a>
     <a href="#">Portafolio</a>
     <a href="#">Sobre mí</a>
     <!-- Agrega más enlaces aquí -->
+</div>
+
 </div>  
 </body>
 </html>
