14a9639473b481342a67378a9a16d5eef16f097b
WanChizu <lizortega747@gmail.com>
2024-05-02 10:58:37 -0400

    crud de pago

---
+++ Laravel-D-Antojitos/CRUDPago/queryActu.php
@@ -0,0 +1,17 @@
+<?php
+
+include_once 'conexion.php';
+$id_pago = $_POST['id_pago'];
+
+if(isset($_POST['id_pago'])){
+
+    $id = $_POST['id_pago'];
+
+    $query = "UPDATE `pagos` SET tipo_pago='$_POST[tipo_pago]', descripcion_pago='$_POST[descripcion_pago]' where id_pago='$_POST[id_pago]'";
+    mysqli_query($mysqli, $query);
+   
+}
+
+header("location: ./index.php?insert=success");
+
+?>
